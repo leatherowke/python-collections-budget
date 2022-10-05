f6229233ffc2f70ad1bb3ec1117d46d08f99c8f5
leatherowke <103121924+leatherowke@users.noreply.github.com>
2022-10-04 22:19:10 -0500

    section 5 done

--- budget/Expense.py
+++ budget/Expense.py
@@ -1,5 +1,7 @@
 import csv
 from datetime import datetime
+from re import X
+from types import new_class
 
 class Expense():
     def __init__(self, date_str, vendor, category, amount):
@@ -40,3 +42,17 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+    
+    def categorize_set_comprehension(self):
+
+        necessary_expenses = { x for x in self.list
+            if x.category == 'Phone' or x.category == 'Auto and Gas' or
+                x.category == 'Classes' or x.category == 'Utilities' or
+                x.category == 'Mortgage'}
+
+        food_expenses = { x for x in self.list
+            if x.category == 'Groceries' or x.category == 'Eating Out'}
+        
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
